5633bc880209bda1930dcc8d83d721b3cbe9b000
Rainbow Dreamer <2180502841@qq.com>
2021-09-14 05:17:40 +0800

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 setup(name='audio_effects',
       packages=find_packages(),
-      version='0.14',
+      version='0.15',
       license='GPLv3',
       description=
       'Some audio effects such as delay, speed changes implemented in python',
@@ -11,7 +11,7 @@ setup(name='audio_effects',
       author_email='1036889495@qq.com',
       url='https://github.com/Rainbow-Dreamer/audio_effects',
       download_url=
-      'https://github.com/Rainbow-Dreamer/audio_effects/archive/0.14.tar.gz',
+      'https://github.com/Rainbow-Dreamer/audio_effects/archive/0.15.tar.gz',
       keywords=['python', 'audio', 'effects'],
       install_requires=['pydub', 'simpleaudio', 'numpy'],
       classifiers=[
